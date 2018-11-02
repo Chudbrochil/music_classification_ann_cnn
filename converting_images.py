ca0981c27c09c951ec2f1b3c69f9387404250269
tglunt <tglunt@unm.edu>
2018-11-02 17:31:10 -0600

    Rewrote building classifier and converting image files.

--- converting_images.py
+++ converting_images.py
@@ -2,56 +2,62 @@ import numpy as np
 from skimage.io import imread
 import os
 
-"""
-X_train = []
-y_train = []
-label = []
-current_wd = os.getcwd()
-for file in os.listdir(current_wd + "/pngfiles"):
-    print(file)
-    image = imread(current_wd + "/pngfiles/" + file)
-    image = image[:, :, :3]
-    image = image.reshape([124, 174, 3])
-    X_train.append(image)
+def main():
+    convert_training_images()
+    convert_validation_images()
 
-    if "blues" in file:
-        label = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    elif "classical" in file:
-        label = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
-    elif "country" in file:
-        label = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
-    elif "disco" in file:
-        label = [0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
-    elif "hiphop" in file:
-        label = [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
-    elif "jazz" in file:
-        label = [0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
-    elif "metal" in file:
-        label = [0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
-    elif "pop" in file:
-        label = [0, 0, 0, 0, 0, 0, 0, 1, 0, 0]
-    elif "reggae" in file:
-        label = [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
-    elif "rock" in file:
-        label = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+def convert_training_images():
+    X_train = []
+    y_train = []
+    label = []
+    current_wd = os.getcwd()
+    for file in os.listdir(current_wd + "/split_pngfiles"):
+        print(file)
+        image = imread(current_wd + "/split_pngfiles/" + file)
+        image = image[:, :, 0]
+        # image = image.reshape([124, 174, 3])
+        X_train.append(image)
 
-    y_train.append(label)
+        if "blues" in file:
+            label = [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        elif "classical" in file:
+            label = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+        elif "country" in file:
+            label = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+        elif "disco" in file:
+            label = [0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+        elif "hiphop" in file:
+            label = [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+        elif "jazz" in file:
+            label = [0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
+        elif "metal" in file:
+            label = [0, 0, 0, 0, 0, 0, 1, 0, 0, 0]
+        elif "pop" in file:
+            label = [0, 0, 0, 0, 0, 0, 0, 1, 0, 0]
+        elif "reggae" in file:
+            label = [0, 0, 0, 0, 0, 0, 0, 0, 1, 0]
+        elif "rock" in file:
+            label = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
 
-print(np.array(X_train).shape)
-print(np.array(y_train).shape)
-np.array(X_train).dump("X_train.dat")
-np.array(y_train).dump("y_train.dat")
-"""
+        y_train.append(label)
 
+    print(np.array(X_train).shape)
+    print(np.array(y_train).shape)
+    np.array(X_train).dump("X_train.dat")
+    np.array(y_train).dump("y_train.dat")
 
-X_test = []
-current_wd = os.getcwd()
-for file in os.listdir(current_wd + "/validation_pngfiles"):
-    print(file)
-    image = imread(current_wd + "/validation_pngfiles/" + file)
-    image = image[:, :, :3]
-    image = image.reshape([124, 174, 3])
-    X_test.append(image)
+def convert_validation_images():
+    X_test = []
+    current_wd = os.getcwd()
+    for file in os.listdir(current_wd + "/validation_split_pngfiles"):
+        print(file)
+        image = imread(current_wd + "/validation_split_pngfiles/" + file)
+        image = image[:, :, 0]
+        # image = image.reshape([124, 174, 3])
+        X_test.append(image)
 
-print(np.array(X_test).shape)
-np.array(X_test).dump("X_test.dat")
+    print(np.array(X_test).shape)
+    np.array(X_test).dump("X_test.dat")
+
+if __name__ == "__main__":
+    main()
